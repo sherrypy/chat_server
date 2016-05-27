f4549607f119bfb6ce275108f2bf07b8d297c397
sherrypy <sherry.xiao@temple.edu>
2016-05-26 23:18:36 -0400

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+def welcome():
+	return '\n｡:.ﾟヽ(｡◕◡◕｡)ﾉﾟ.:｡+ﾟ\n'
+	
+def highFive():
+	return '\n╭(●｀∀´●)╯╰(●’◡’●)╮\n'
+
+def fight():
+	return '\n(ง •̀_•́ )ง\n'
+
+def lieDown():
+	return '\n_(:з」∠)_ _\n'
+
+def confuse():
+	return '\n（⊙.⊙）?\n'
+
+def love():
+	return '\n( ˘ ³˘)♥\n'
+
+def cry():
+	return '\n╥﹏╥\n'
+
+def angry():
+	return '\n╰（‵□′）╯\n'
+
+def happy():
+	return '\n╰（￣▽￣）╭\n'
+
+def awkward():
+	return '\n(“▔□▔)\n'
